517344a6d2ac4aa5f189705a81395aac7b06f8da
andrescortesg1 <andres.cortesg@udea.edu.co>
2025-04-25 11:24:46 -0500

    BATMAN

---
+++ src/test/java/com/udea/labfaker/LabfakerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udea.labfaker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabfakerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
